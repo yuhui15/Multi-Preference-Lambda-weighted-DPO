7acad7f3aa9aa227533f882fe25d884ac2d89be7
yuhui15 <155590543+yuhui15@users.noreply.github.com>
2025-06-10 13:51:57 +0800

    Update workflow.py

--- lambda_dpo/src/llamafactory/train/dpo/workflow.py
+++ lambda_dpo/src/llamafactory/train/dpo/workflow.py
@@ -1,23 +1,8 @@
 # Copyright 2025 HuggingFace Inc. and the LlamaFactory team.
-#
-# This code is inspired by the HuggingFace's TRL library.
-# https://github.com/huggingface/trl/blob/v0.8.0/examples/scripts/dpo.py
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Extended to support lambda_dpo while preserving existing logic
 
 from typing import TYPE_CHECKING, Optional
 
-from ...data import PairwiseDataCollatorWithPadding, get_dataset, get_template_and_fix_tokenizer
 from ...extras.constants import IGNORE_INDEX
 from ...extras.misc import calculate_tps
 from ...extras.ploting import plot_loss
@@ -25,13 +10,11 @@ from ...hparams import ModelArguments
 from ...model import load_model, load_tokenizer
 from ..trainer_utils import create_modelcard_and_push, create_ref_model
 from .trainer import CustomDPOTrainer
-
+from ...data import get_dataset, get_template_and_fix_tokenizer
 
 if TYPE_CHECKING:
     from transformers import Seq2SeqTrainingArguments, TrainerCallback
-
     from ...hparams import DataArguments, FinetuningArguments
-
 
 def run_dpo(
     model_args: "ModelArguments",
@@ -46,24 +29,35 @@ def run_dpo(
     dataset_module = get_dataset(template, model_args, data_args, training_args, stage="rm", **tokenizer_module)
     model = load_model(tokenizer, model_args, finetuning_args, training_args.do_train)
 
-    data_collator = PairwiseDataCollatorWithPadding(
-        template=template,
-        model=model,
-        pad_to_multiple_of=8,
-        label_pad_token_id=IGNORE_INDEX if data_args.ignore_pad_token_for_loss else tokenizer.pad_token_id,
-        **tokenizer_module,
-    )
+    # Dynamically choose collator based on pref_loss
+    if finetuning_args.pref_loss == "lambda_dpo":
+        from ...data import ListwiseDataCollatorWithPadding
+        data_collator = ListwiseDataCollatorWithPadding(
+            template=template,
+            model=model,
+            pad_to_multiple_of=8,
+            label_pad_token_id=IGNORE_INDEX if data_args.ignore_pad_token_for_loss else tokenizer.pad_token_id,
+            **tokenizer_module,
+        )
+    else:
+        from ...data import PairwiseDataCollatorWithPadding
+        data_collator = PairwiseDataCollatorWithPadding(
+            template=template,
+            model=model,
+            pad_to_multiple_of=8,
+            label_pad_token_id=IGNORE_INDEX if data_args.ignore_pad_token_for_loss else tokenizer.pad_token_id,
+            **tokenizer_module,
+        )
 
     # Create reference model
     if finetuning_args.use_ref_model:
-        if finetuning_args.ref_model is None and (not training_args.do_train):  # use the model itself
+        if finetuning_args.ref_model is None and (not training_args.do_train):
             ref_model = model
         else:
             ref_model = create_ref_model(model_args, finetuning_args)
     else:
         ref_model = None
 
-    # Initialize our Trainer
     trainer = CustomDPOTrainer(
         model=model,
         ref_model=ref_model,
@@ -75,7 +69,6 @@ def run_dpo(
         **tokenizer_module,
     )
 
-    # Training
     if training_args.do_train:
         train_result = trainer.train(resume_from_checkpoint=training_args.resume_from_checkpoint)
         trainer.save_model()
@@ -96,15 +89,13 @@ def run_dpo(
 
             plot_loss(training_args.output_dir, keys=keys)
 
-    # Evaluation
     if training_args.do_eval:
         metrics = trainer.evaluate(metric_key_prefix="eval")
-        if id(model) == id(ref_model):  # unable to compute rewards if reference model is the model itself
+        if id(model) == id(ref_model):
             remove_keys = [key for key in metrics.keys() if "rewards" in key]
             for key in remove_keys:
                 metrics.pop(key)
         trainer.log_metrics("eval", metrics)
         trainer.save_metrics("eval", metrics)
 
-    # Create model card
     create_modelcard_and_push(trainer, model_args, data_args, training_args, finetuning_args)
